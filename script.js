3e53716f325f8f32bcdefa9278a95d9d29f0a130
kovacs-kacsozs <kovacs-kacsozs.24d@acsjszki.hu>
2025-03-06 11:30:11 +0100

    1

---
+++ script.js
@@ -0,0 +1,31 @@
+
+var quanity prompt('add meg a darabszámot');
+var type = prompt('add meg a jegy típust');
+
+console.log(quanity);
+console.log(type);
+
+var price = retired === 'student' ? 300 : 350;
+var discount = quanity > 10 ? 0.9 : 1;
+
+var total = price * quanity * discount;
+
+var types {
+student: 300,
+adult: 350,
+retired: 280
+};
+
+alert("üzenet");
+
+/* 
+
+input:
+milyen tipusú jegyet kér? (student | adult)
+hány jegyet kér?
+
+a diák jegy ára 300 Ft
+a felnőtt jegy ára 350 Ft
+a nyugdijas jegy ára 280 Ft
+tíznál több jegy esetén 10% kedvezmény
+*/
